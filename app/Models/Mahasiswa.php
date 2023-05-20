d7b25e3218a6f8b74242b8b75cf0557fea57c7b1
Agung Malik Al Qindy <agungalqindy@gmail.com>
2023-05-20 19:12:30 +0700

    praktikum_2-5

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    protected $table = "mahasiswas";
+    public $timestamps = false;
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'tanggal_lahir',
+        'jurusan',
+        'email',
+        'no_hp',
+        'kelas_id',
+    ];
+}
